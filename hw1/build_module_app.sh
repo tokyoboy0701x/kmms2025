6c9d800cb3a53afa85053303a8f10076110c9c81
tokyoboy0701x <sotnikovivan3000@gmail.com>
2025-10-01 16:19:18 +0300

    changed build script

--- hw1/build_module_app.sh
+++ hw1/build_module_app.sh
@@ -1,9 +1,14 @@
 CPP_FILES="main.cpp shell_sort.cpp smooth_generator.cpp"
-APP=example
+APP=example 
+FOLDER=bin
+
+mkdir -p $FOLDER
+cd $FOLDER
 
 if [ -f $APP ]; then rm $APP
 fi
 
-g++ $CPP_FILES -o $APP
+cd ..
+g++ $CPP_FILES -o $FOLDER/$APP
 
-./$APP
+./$FOLDER/$APP
