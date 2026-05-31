5f30b128fb765a54ac8bf1224b1585e1b9fe5a30
tokyoboy0701x <sotnikovivan3000@yandex.ru>
2026-05-31 15:27:55 +0300

    game objects - init and collision

--- super_mario_1/super_mario_raw.cpp
+++ super_mario_1/super_mario_raw.cpp
@@ -76,7 +76,6 @@ void Sleep(int ms) {
     usleep(ms * 1000);
 }
 
-
 void ClearMap() {
     for (int i = 0; i < mapWidth; i++)
         map[0][i] = ' ';
@@ -92,3 +91,36 @@ void ShowMap() {
     }
 }
 
+void SetObjectPos(TObject *obj, float xPos, float yPos) {
+    obj->x = xPos;
+    obj->y = yPos;
+}
+
+void InitObject(TObject *obj, float xPos, float yPos, float oWidth, float oHeight, char inType) {
+    SetObjectPos(obj, xPos, yPos);
+    obj->width  = oWidth;
+    obj->height = oHeight;
+    obj->vertSpeed = 0;
+    obj->IsFly = false;
+    obj->cType  = inType;
+}
+
+bool IsCollision(TObject o1, TObject o2) {
+    return ((o1.x + o1.width)  > o2.x) && (o1.x < (o2.x + o2.width)) &&
+           ((o1.y + o1.height) > o2.y) && (o1.y < (o2.y + o2.height));
+}
+
+bool IsPosInMap(int x, int y) {
+    return (x >= 0) && (x < mapWidth) && (y >= 0) && (y < mapHeight);
+}
+
+void PutObjectOnMap(TObject obj) {
+    int ix = (int)round(obj.x);
+    int iy = (int)round(obj.y);
+    int iWidth  = (int)round(obj.width);
+    int iHeight = (int)round(obj.height);
+    for (int i = ix; i < ix + iWidth; i++)
+        for (int j = iy; j < iy + iHeight; j++)
+            if (IsPosInMap(i, j))
+                map[j][i] = obj.cType;
+}
