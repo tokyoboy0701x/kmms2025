3c9cbb9dfdbabc3cf905c89a7842903da343119a
tokyoboy0701x <sotnikovivan3000@yandex.ru>
2026-05-31 15:34:41 +0300

    main game loop

--- super_mario_1/super_mario_raw.cpp
+++ super_mario_1/super_mario_raw.cpp
@@ -180,3 +180,41 @@ void CreateLevel(int lvl) {
     }
 }
 
+int main() {
+    enableRawMode();
+    atexit(disableRawMode);
+
+    printf("\033[2J");
+
+    CreateLevel(level);
+
+    do {
+        key_a = false;
+        key_d = false;
+        pollKeys();
+
+        ClearMap();
+
+        if (!mario.IsFly && key_space) mario.vertSpeed = -1.0f;
+        if (key_a) HorizonMoveMap( 1.0f);
+        if (key_d) HorizonMoveMap(-1.0f);
+
+        if (mario.y > mapHeight) CreateLevel(level);
+
+        VertMoveObject(&mario);
+        for (int i = 0; i < brickLength; i++)
+            PutObjectOnMap(brick[i]);
+        PutObjectOnMap(mario);
+
+        setCur(0, 0);
+        ShowMap();
+        fflush(stdout);
+
+        Sleep(10);
+
+    } while (!key_esc);
+
+    free(brick);
+    printf("\033[2J\033[H");
+    return 0;
+}
