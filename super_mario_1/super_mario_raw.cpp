cb4d25acd35f6df3e7c72703e2853f25e86686d3
tokyoboy0701x <sotnikovivan3000@yandex.ru>
2026-05-31 15:32:49 +0300

    physics - vertical and horizontal movement

--- super_mario_1/super_mario_raw.cpp
+++ super_mario_1/super_mario_raw.cpp
@@ -124,3 +124,35 @@ void PutObjectOnMap(TObject obj) {
             if (IsPosInMap(i, j))
                 map[j][i] = obj.cType;
 }
+
+void VertMoveObject(TObject *obj) {
+    obj->IsFly = false;
+    obj->vertSpeed += 0.05f;
+    SetObjectPos(obj, obj->x, obj->y + obj->vertSpeed);
+    for (int i = 0; i < brickLength; i++) {
+        if (IsCollision(*obj, brick[i])) {
+            obj->y -= obj->vertSpeed;
+            obj->vertSpeed = 0;
+            obj->IsFly = false;
+            if (brick[i].cType == '+') {
+                level++;
+                if (level > 2) level = 1;
+                CreateLevel(level);
+                Sleep(1000);
+            }
+            break;
+        }
+    }
+}
+
+void HorizonMoveMap(float dx) {
+    mario.x -= dx;
+    for (int i = 0; i < brickLength; i++)
+        if (IsCollision(mario, brick[i])) {
+            mario.x += dx;
+            return;
+        }
+    mario.x += dx;
+    for (int i = 0; i < brickLength; i++)
+        brick[i].x += dx;
+}
