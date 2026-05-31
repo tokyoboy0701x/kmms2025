17ab623622017f2e8ffcb06c241ccf348ccd4885
tokyoboy0701x <sotnikovivan3000@yandex.ru>
2026-05-31 15:24:48 +0300

    terminal raw mode and input handling

--- super_mario_1/super_mario_raw.cpp
+++ super_mario_1/super_mario_raw.cpp
@@ -23,3 +23,47 @@ TObject mario;
 TObject *brick = NULL;
 int brickLength;
 int level = 1;
+
+static struct termios orig_termios;
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 0;
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    printf("\033[?25l");
+    fflush(stdout);
+}
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    printf("\033[?25h");
+    fflush(stdout);
+}
+
+int getKey() {
+    unsigned char c;
+    if (read(STDIN_FILENO, &c, 1) == 1)
+        return (int)c;
+    return -1;
+}
+
+static bool key_a = false;
+static bool key_d = false;
+static bool key_space = false;
+static bool key_esc = false;
+
+void pollKeys() {
+    key_space = false;
+    key_esc = false;
+
+    int c;
+    while ((c = getKey()) != -1) {
+        if (c == 27)  key_esc = true;
+        if (c == ' ') key_space = true;
+        if (c == 'a' || c == 'A') key_a = true;
+        if (c == 'd' || c == 'D') key_d = true;
+    }
+}
